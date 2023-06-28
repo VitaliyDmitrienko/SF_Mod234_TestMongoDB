61136fd80c3937fc051998403368af110fd09921
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-06-28 18:07:52 +0300

    1st.

---
+++ src/main/java/org/example/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findByFirstName(String firstName);
+    public List<User> findByEmail(String email);
+    Long deletePersonByfirstName(String firstName);
+}
